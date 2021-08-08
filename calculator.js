f4d4d86dedd40c70943fda2dc09ae5311b6537ea
Andrews-Mensah <82477892+Andrews-Mensah@users.noreply.github.com>
2021-08-08 22:28:14 +0000

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,14 @@
+class Calculator {
+    constructor (para1,para2){
+       this.para1 = para1;
+       this.para2 = para2; 
+    }
+    add(){
+        return para1 + para2;
+    }
+
+    product(){
+        return para1 * para2;
+
+    }
+}
---
+++ main.js
@@ -0,0 +1,3 @@
+let sum = new Calculator (5,7);
+let result = sum.add();
+console.log(result);
